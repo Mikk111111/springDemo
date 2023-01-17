b851c6759623e05eb9cc9c18ec12a4451ab21f2f
mikk111111 <mik.k00259@gmail.com>
2023-01-17 16:54:56 +0200

    local waorking page two

--- src/main/java/com/organization/springDemo/repository/controller/CustomerTemplateController.java
+++ src/main/java/com/organization/springDemo/repository/controller/CustomerTemplateController.java
@@ -1,9 +1,12 @@
 package com.organization.springDemo.repository.controller;
 
+import com.organization.springDemo.repository.model.Customer;
 import com.organization.springDemo.repository.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +18,14 @@ public class CustomerTemplateController {
     @GetMapping(path="/test")
     public String get(){
         return "test/test_page";
+    }
+    //http://localhost:8080/customertemplate/firspage/customer/112
+    @GetMapping(path="/firspage/customer/{id}")
+    public String getFirstPage(Model model, @PathVariable int id){
+        Customer customer = customerService.getMyCustomerByID(id);
+        model.addAttribute("company_name",customer.getContactFirstName());
+        model.addAttribute("contact_first_name",customer.getContactLastName());
+        model.addAttribute("country",customer.getCountry());
+        return "test/firstpage";
     }
 }
